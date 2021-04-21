757c45663c5afb3e178d48a87b32f3cb36198218
student <rajaboina.ranjith@hcl.com>
2021-04-21 08:21:15 +0000

    first commit

---
+++ src/main/java/com/devops/gradedassignment/App.java
@@ -0,0 +1,13 @@
+package com.devops.gradedassignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops" );
+    }
+}
